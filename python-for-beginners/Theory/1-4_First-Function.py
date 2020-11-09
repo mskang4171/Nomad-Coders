6e260dc7a3b8bce2990ed4ef0231f9c198020c40
Minsoo Kang <mskang4171@gmail.com>
2020-11-09 19:36:57 +0900

    Revert "python theory"
    This reverts commit 2b4a39bef3fd14908a968f18ab9016aafcef7a37.

--- python-for-beginners/Theory/1-4_First-Function.py
+++
@@ -1,7 +0,0 @@
-def say_hello():
-    print("hello")
-    print("bye")
-
-
-say_hello()
-say_hello  # 함수가 실행되지 않는다.
--- python-for-beginners/Theory/1-9_Conditionals.py
+++
@@ -1,8 +0,0 @@
-def plus(a, b):
-    if type(b) is int or type(b) is float:
-        return a+b
-    else:
-        return None
-
-
-print(plus(12, "10"))
